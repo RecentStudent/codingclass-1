5ec5a542aec1da921e4b71050a78633a360808da
RecentStudent <Pikachuwave@yahoo.com>
2015-03-06 15:22:09 -0800

    Update 5-each_iteration.rb

--- ruby/lessons/9-Loops/5-each_iteration.rb
+++ ruby/lessons/9-Loops/5-each_iteration.rb
@@ -10,20 +10,22 @@ array = [0, 1, 2, 3, 4, 5]
 # array by calling .each
 
 array.each do |item|
-  puts "The current array item is: #{item}"
+  puts "the current array item is #{item}"
 end
 
-# We can call "item" anything we want:
 
-array.each do |this_is_a_really_long_variable|
-  puts "The current array item is: #{this_is_a_really_long_variable}"
+# We can call "item" anything we want:
+array.each do |itm|
+  puts "the current array itm is #{itm}"
 end
 
 
 # Having the do..end is called a block. We can 
 # write this shorthand as:
 
-array.each { |item| puts "The current array item is: #{item}" }
+array.each { |item| puts "the cuurrent item is #{item}"
+
+
 
 # It is conventional to write blocks using 
 # the do...end syntax for multiple lines of code 
@@ -32,22 +34,10 @@ array.each { |item| puts "The current array item is: #{item}" }
 
 # We can also manipulate items inside the block
 # without changing the original array.
-
 array.each do |item|
-  item += 2
-  puts "The array item plus two: #{item}"
+  item += 1
+  puts item
 end
-
-puts array.inspect
-
-
-
-
-
-
-
-
-
 
 
 
